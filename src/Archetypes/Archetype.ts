4e86a83f2cc75facc5dd6861d5e612fadc224969
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-03-15 16:42:49 -0300

    req 04 - criando Archetype class - Done

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,35 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(
+    name: string,
+  ) { 
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');    
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
